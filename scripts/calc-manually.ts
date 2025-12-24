7245ede670ad3bf287236ccfe0935ee0ee49b6d7
JohnWon <1498118157@qq.com>
2025-12-25 01:52:10 +0800

    wip: calc manually
    Signed-off-by: JohnWon <1498118157@qq.com>

--- scripts/calc-manually.ts
+++ scripts/calc-manually.ts
@@ -3,7 +3,9 @@
 // tick lower: 1 eth = 4545 usdc
 // tick upper: 1 eth = 5500 usdc
 
-const ETH_AMOUNT = 1
+const ETH = 10 ** 18
+const ETH_AMOUNT = 1 * ETH
+const USDC_AMOUNT = 5000 * ETH
 const CURRENT_PRICE = 5000
 const MIN_PRICE = 4545
 const MAX_PRICE = 5500
@@ -48,7 +50,7 @@ console.log({
 console.log('Calculate Lx Ly liquidity ------------------------------------')
 
 const Lx = ETH_AMOUNT * (((currentSqrtPQ96 * upperSqrtPQ96) / Q96) / (upperSqrtPQ96 - currentSqrtPQ96))
-const Ly = CURRENT_PRICE * Q96 / (currentSqrtPQ96 - lowerSqrtPQ96)
+const Ly = USDC_AMOUNT * Q96 / (currentSqrtPQ96 - lowerSqrtPQ96)
 const Lf = Math.min(Lx, Ly)
 
 console.log({
@@ -67,8 +69,23 @@ console.log({
   y,
 })
 
+console.log('Calculate first transaction ------------------------------------')
+
+const AMOUNT_IN = 42
+const amountInP = (AMOUNT_IN * Q96) / Lf
+const newPriceQ96 = currentSqrtP + amountInP
+const newPrice = (newPriceQ96 / Q96) ** 2
+
+console.log({
+  amountInP,
+  newPriceQ96,
+  newPrice,
+})
+
 // util
 function getLogBase(n: number, b: number) {
   // log_b(n) = log_e(n) / log_e(b)
   return Math.log(n) / Math.log(b)
 }
+
+export {}
