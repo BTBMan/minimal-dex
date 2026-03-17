d03ac896ad425dba7255132d15c5ca6babb4cd32
JohnWon <1498118157@qq.com>
2026-03-17 16:15:27 +0800

    fix: CREATE2 address calculation
    Signed-off-by: JohnWon <1498118157@qq.com>

--- contract/src/libraries/PoolAddress.sol
+++ contract/src/libraries/PoolAddress.sol
@@ -16,24 +16,31 @@ library PoolAddress {
     /**
      * @notice Compute the pool address
      * @param factory The address of the factory contract
-     * @param tokenA The address of the first token
-     * @param tokenB The address of the second token
+     * @param token0 The address of the first token
+     * @param token1 The address of the second token
      * @param tickSpacing The tick spacing
-     * @return address The address of the pool contract
+     * @return pool The address of the pool contract
      */
     function computeAddress(address factory, address token0, address token1, uint24 tickSpacing)
         internal
+        pure
         returns (address pool)
     {
         // Ensure token0 less than token1
         require(token0 < token1);
 
-        pool = keccak256(
-            abi.encodePacked(
-                "0xff",
-                factory,
-                keccak256(abi.encodePacked(token0, token1, tickSpacing)),
-                keccak256(type(Pool).creationCode)
+        pool = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            "0xff",
+                            factory,
+                            keccak256(abi.encodePacked(token0, token1, tickSpacing)),
+                            keccak256(type(Pool).creationCode)
+                        )
+                    )
+                )
             )
         );
     }
